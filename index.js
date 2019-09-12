0efbb9a92b5438ff1a8e0b0eb6f291f7953108bb
douglaspoa <dodosobreira@gmail.com>
2019-09-12 17:50:58 -0300

    pegar porta do servidor

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
+var porta = process.env.PORT || 8080;
+app.listen(porta);
+
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
 app.post('/users', db.createUser)
